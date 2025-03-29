ed0725f31b866bfdfdb5c67f267d0d44c6815b2e
Sadnyani Gomkar <sadnyanig@gmail.com>
2025-03-29 19:12:39 +0530

    added build repository for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import UserList from './components/Users/UserList';
 import UserEdit from './components/Users/UserEdit';
